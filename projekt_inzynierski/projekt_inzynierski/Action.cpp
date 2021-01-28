52cc7ef8b09304c85992ac05bc27efa9d9ca27af
lewandd <45177176+lewandd@users.noreply.github.com>
2021-01-28 06:01:42 +0100

    Zmiana konstruktora Action

--- projekt_inzynierski/projekt_inzynierski/Action.cpp
+++ projekt_inzynierski/projekt_inzynierski/Action.cpp
@@ -1,10 +1,3 @@
 #include "Action.h"
 
-Action::Action(int mode_, int octree_, int x_, int y_, int z_, int type_) {
-    mode = mode_;
-    octree = octree_;
-    x = x_;
-    y = y_;
-    z = z_;
-    type = type_;
-}
+Action::Action(int mode_, int octree_, int x_, int y_, int z_, int type_) : mode(mode_), octree(octree_), x(x_), y(y_), z(z_), type(type_) {}
